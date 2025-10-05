2e90ae4ea24f4758432ecdccd4e0412c19097ef8
Astitva <astitvabartaria@gmail.com>
2025-10-05 15:57:57 +0530

    added cors origin again

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://twig-go-frontend.vercel.app"],
+    origin: ["http://localhost:5173", "https://twiggo.vercel.app"],
     credentials: true,
   })
 );
--- src/lib/socket.js
+++ src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://twig-go-frontend.vercel.app"],
+    origin: ["http://localhost:5173", "https://twiggo.vercel.app"],
   },
 });
 
